610a6b8f9b65325206451ab39c794baa7637b994
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-12-01 09:39:14 -0500

    updates to admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -68,7 +68,6 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
-
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
